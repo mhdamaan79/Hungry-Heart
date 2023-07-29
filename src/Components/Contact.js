e98570b9364b6353b7b15ab6b0b10f53d0b1aa3d
Mohd Amaan <mhdamaan79@gmail.com>
2023-07-29 09:52:50 +0530

    Contact button text updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -80,7 +80,7 @@ const Contact = () => {
                   class="rounded relative inline-flex group items-center justify-center px-3.5 py-2 mt-2 cursor-pointer border-b-4 border-l-2 active:border-purple-600 active:shadow-none shadow-lg bg-gradient-to-tr from-purple-600 to-purple-500 border-purple-700 text-white"
                 >
                   <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
-                  <span class="relative">Clear Cart</span>
+                  <span class="relative">Submit</span>
                 </a>
               </button>
             </div>
